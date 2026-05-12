6525dd5a14414ac7b7ade54531428085a3906434
AMER LARDHI <amersalmeen@outlook.sa>
2026-05-12 22:02:24 +0300

    Ignored spaces in counting

--- challenge1.py
+++ challenge1.py
@@ -2,6 +2,8 @@ sentence = input("Enter sentence: ").upper()
 letter = input("Enter letter: ").upper()
 count = 0
 for l in sentence:
+    if l == " ":
+        continue
     if l == letter:
         count += 1
 print("The Letter", letter, "appears", count)
