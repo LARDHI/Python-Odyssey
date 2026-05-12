e9dd15c6289dded851c8941fd7b22ce2b6b5c1e8
AMER LARDHI <amersalmeen@outlook.sa>
2026-05-12 22:25:39 +0300

    Added most repeated character feature

--- challenge1.py
+++ challenge1.py
@@ -1,10 +1,18 @@
 sentence = input("Enter sentence: ").upper()
 letter = input("Enter letter: ").upper()
 count = 0
+most_char = None
+most_count = 0
+z = []
 for l in sentence:
     if l == " ":
         continue
     if l == letter:
         count += 1
+    z.append(l)
+    if z.count(l) > most_count:
+        most_count = z.count(l)
+        most_char = l
 print("The Letter", letter, "appears", count)
+print("The Most Counted Character is", most_char, 'Counted', most_count, 'Times.')
 print("Author: Amer Lardhi")
