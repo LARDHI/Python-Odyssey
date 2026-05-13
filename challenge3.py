c5623486781120217bda3c4ef36e32217366b8f7
AMER LARDHI <amersalmeen@outlook.sa>
2026-05-13 18:00:27 +0300

    Build Functon for checking The Issues

--- challenge3.py
+++ challenge3.py
@@ -1,24 +1,19 @@
+def check_password(pw):
+    issue = []
+    if " " in pw:
+        issue.append("The Password Must not Have Spaces")
+    if len(pw) <= 8:
+        issue.append("The Password Must be 8 character at Least")
+    if '@' not in pw:
+        issue.append("The Password Must Have @ in it")
+    return issue
 print("\n\nWelcome in Password Checker!\n")
 while True:
-    counter = 3
     password = input("Enter Your Password: ")
-    if " " in password or len(password) < 9 or "@" not in password:
-        if " " in password:
-            print("The Password Must Not Have Space")
-            counter -= 1
-        if len(password) < 9:
-            print('The Password Must be 8 character at Least')
-            counter -= 1
-        if "@" not in password:
-            print("The Password Must have @ in it")
-            counter -= 1
-
-        if counter == 2:
-            print("Your Password is Medium")
-        elif counter == 1:
-            print("Your Password is Weak")
-        else: print("Try Another Password!")
-        continue
-    else: 
-        print(f'Your Password: {password} is Strong and Passed!')
+    issues = check_password(password)
+    for issue in issues:
+        print(issue)
+    if len(issues) == 0:
+        print("Well Done!")
         break
+print(f'Your Password: {password} is Strong and Passed!')
