4a3aced33f443130b7caeb1431262718a2987701
AMER LARDHI <amersalmeen@outlook.sa>
2026-05-13 20:15:47 +0300

    Upgrading And Improve The code of Challenge3

--- challenge3.py
+++ challenge3.py
@@ -3,17 +3,24 @@ def check_password(pw):
     if " " in pw:
         issue.append("The Password Must not Have Spaces")
     if len(pw) <= 8:
-        issue.append("The Password Must be 8 character at Least")
-    if '@' not in pw:
+        issue.append("The Password Must be 8 characters at least")
+    if "@" not in pw:
         issue.append("The Password Must Have @ in it")
     return issue
+
 print("\n\nWelcome in Password Checker!\n")
+
 while True:
     password = input("Enter Your Password: ")
     issues = check_password(password)
+    if len(issues) == 0:
+        print("Your Password Level Is Strong")
+        break
     for issue in issues:
         print(issue)
-    if len(issues) == 0:
-        print("Well Done!")
-        break
-print(f'Your Password: {password} is Strong and Passed!')
+    if len(issues) == 1:
+        print("Your Password Level Is Medium")
+    elif len(issues) == 2:
+        print("Your Password Level Is Weak")
+    else:
+        print("Your Password Level Is Very Weak")
