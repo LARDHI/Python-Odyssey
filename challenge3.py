222eac1668cc0aad090cba68e0e6b5c823ee6ca0
AMER LARDHI <amersalmeen@outlook.sa>
2026-05-13 16:42:11 +0300

    Improve Challenge3.py Logic

--- challenge3.py
+++ challenge3.py
@@ -1,13 +1,24 @@
 print("\n\nWelcome in Password Checker!\n")
 while True:
+    counter = 3
     password = input("Enter Your Password: ")
-    if " " in password:
-        print("The Password Must not Have space in it")
+    if " " in password or len(password) < 9 or "@" not in password:
+        if " " in password:
+            print("The Password Must Not Have Space")
+            counter -= 1
+        if len(password) < 9:
+            print('The Password Must be 8 character at Least')
+            counter -= 1
+        if "@" not in password:
+            print("The Password Must have @ in it")
+            counter -= 1
+
+        if counter == 2:
+            print("Your Password is Medium")
+        elif counter == 1:
+            print("Your Password is Weak")
+        else: print("Try Another Password!")
         continue
-    elif len(password) < 9:
-        print("The Password must be 8 character at Least")
-        continue
-    elif "@" not in password:
-        print("The Password Must have @ in it")
-        continue
-    else: break
+    else: 
+        print(f'Your Password: {password} is Strong and Passed!')
+        break
