5e464542d17731918f2d522c0c61e2b3466b03ac
AMER LARDHI <amersalmeen@outlook.sa>
2026-05-16 12:08:47 +0300

    Adding File Reading Feature

--- challenge1.py
+++ challenge1.py
@@ -1,17 +1,24 @@
-sentence = input("Enter sentence: ").upper()
+def is_file(sentence):
+    try:
+        file_content = open(sentence)
+        return file_content.read().upper()
+    except:
+        return sentence
+sentence = input("Enter File Name or Sentence: ")
+content = is_file(sentence)
 letter = input("Enter letter: ").upper()
 count = 0
 most_char = None
 most_count = 0
-z = []
-for l in sentence:
+seen_letter = []
+for l in content:
     if l == " ":
         continue
     if l == letter:
         count += 1
-    z.append(l)
-    if z.count(l) > most_count:
-        most_count = z.count(l)
+    seen_letter.append(l)
+    if seen_letter.count(l) > most_count:
+        most_count = seen_letter.count(l)
         most_char = l
 print("The Letter", letter, "appears", count)
 print("The Most Counted Character is", most_char, 'Counted', most_count, 'Times.')
